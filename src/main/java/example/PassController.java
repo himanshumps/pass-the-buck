c6ec27bffc88209317c59aa4ecc7d47d2e49884d
Himanshu Gupta <himanshu.gupta14@aexp.com>
2020-02-25 09:37:42 +0530

    organize imports

--- src/main/java/example/PassController.java
+++ src/main/java/example/PassController.java
@@ -1,29 +1,11 @@
 package example;
 
-import org.apache.http.HeaderElement;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.conn.ConnectionKeepAliveStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.message.BasicHeaderElementIterator;
-import org.apache.http.protocol.HTTP;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import reactor.core.publisher.Mono;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class PassController {
@@ -81,7 +63,7 @@ public class PassController {
     @Value("${upstream}")
     private String upstream;
 
-    public PassController(){
+    public PassController() {
         WebClient.Builder webClientBuilderInstance = WebClient
                 .builder()
                 .baseUrl(upstream);
