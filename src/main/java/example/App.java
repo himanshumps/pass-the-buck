6ccd458b41285c7b00b8715561a2059a8e609341
Himanshu Gupta <himanshu.gupta14@aexp.com>
2020-02-25 09:38:22 +0530

    enabled webflux

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -2,8 +2,10 @@ package example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class App {
 
     public static void main(String[] args) {
